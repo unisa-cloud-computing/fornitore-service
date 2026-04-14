aab27e4543e3bd04c4835a2a03a01cfd00e8c103
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2026-04-14 21:39:48 +0200

    Initial commit

---
+++ src/main/java/unisa/poultryfarm/fornitoreservice/FornitoreServiceApplication.java
@@ -0,0 +1,13 @@
+package unisa.poultryfarm.fornitoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornitoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FornitoreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unisa/poultryfarm/fornitoreservice/FornitoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package unisa.poultryfarm.fornitoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornitoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
